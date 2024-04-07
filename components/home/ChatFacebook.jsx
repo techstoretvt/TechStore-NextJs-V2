542db4f3c35ba682ea4c847b0af9317570ea1d10
techstoretvt <techstoretvt2565@gmail.com>
2024-04-07 15:09:51 +0700

    v24.4.7-7

--- components/home/ChatFacebook.jsx
+++ components/home/ChatFacebook.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect } from "react";
 import dynamic from 'next/dynamic';
-import { FacebookProvider } from 'react-facebook';
-const { CustomChat } = dynamic(
-  () => import('react-facebook'),
-  { loading: () => <p>Loading...</p> }
-);
+import { FacebookProvider, CustomChat } from 'react-facebook';
+
 
 const id = "116661344652051";
 const appid = "1131387264234227";
